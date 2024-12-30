937bfeb65ab608fd0c1aaba9f904073429cb8320
Keven Carneiro <kevencarneiro@outlook.com>
2024-12-30 05:39:51 -0300

    Fixing inline script

--- index.ts
+++ index.ts
@@ -11,7 +11,7 @@ export default function clarityIntegration(options: ClarityOptions): AstroIntegr
     hooks: {
       'astro:config:setup': ({injectScript}) => {
         if ((options?.enabled ?? true) && options?.projectId) {
-          injectScript('head-inline', `function(c,l,a,r,i,t,y){ c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)}; t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i; y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y); })(window, document, "clarity", "script", "${options.projectId}");`);
+          injectScript('head-inline', `(function(c,l,a,r,i,t,y){ c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)}; t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i; y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y); })(window, document, "clarity", "script", "${options.projectId}");`);
         }
       }
     }
